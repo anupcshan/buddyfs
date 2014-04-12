b7d90192eae7b9b351086baabb7239425b6554bf
Anup Chenthamarakshan <anupcshan@gmail.com>
2014-04-12 11:02:46 -0700

    Remove redundant code.

--- kad_server/buddyd.py
+++ kad_server/buddyd.py
@@ -1,19 +1,17 @@
-import sys, os
+import sys
 import os.path
 import twisted
 sys.path.append(os.path.abspath(os.path.dirname(__file__) + '/..'))
 import settings
 from kad_server.buddynode import BuddyNode
-from entangled.kademlia.datastore import SQLiteDataStore
 
 if __name__ == '__main__':
-	print "Starting buddy-daemon on port " , settings.BUDDY_PORT
+    print "Starting buddy-daemon on port " , settings.BUDDY_PORT
 
-	" Check if the node already has a Node ID, create one if not present "
-	datastore = SQLiteDataStore(dbFile = settings.DBPATH+'/buddydht.db')
-	node = BuddyNode(id, settings.BUDDY_PORT, datastore)
-	
-	" Check DHT for previously connected peers. Next step, check with trackers for the last connected user and get the peer list "
+    " Check if the node already has a Node ID, create one if not present "
+    BuddyNode.get_node()
+    
+    " Check DHT for previously connected peers. Next step, check with trackers for the last connected user and get the peer list "
 
-	twisted.internet.reactor.run()
+    twisted.internet.reactor.run()
 
