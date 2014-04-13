667d9f38d7441faf72fb823a7c6dea22d0643b44
Anup Chenthamarakshan <anupcshan@gmail.com>
2014-04-12 21:07:34 -0700

    Option to select and test access to PGP key at mount time.

--- fs/buddyfs.py
+++ fs/buddyfs.py
@@ -217,10 +217,27 @@ class BuddyFSOperations(llfuse.Operations):
         super(BuddyFSOperations, self).__init__()
         self.tree = FSTree()
         self.gpg = gnupg.GPG()
+        self.test_key(key_id)
+
+    def test_key(self, key_id):
         self.gpg_key = filter (lambda x :
                 x.get('keyid').find(key_id) >= 0, self.gpg.list_keys(True))
         if len(self.gpg_key) != 1:
             raise 'Invalid or non-existent GPG key specified'
+
+        self.gpg_key = self.gpg_key[0]
+        test_encrypt = self.gpg.encrypt('0xDEADBEEF', self.gpg_key['fingerprint'])
+        
+        if not test_encrypt.ok:
+            raise 'Unable to encrypt to provided fingerprint'
+
+        test_decrypt = self.gpg.decrypt(test_encrypt.data)
+
+        if not test_decrypt.ok:
+            raise 'Unable to decrypt messages to provided fingerprint'
+
+        if test_decrypt.data != '0xDEADBEEF':
+            raise 'GPG binaries unable to encrypt or decrypt accurately'
 
     def statfs(self):
         stat_ = llfuse.StatvfsData()
