e109ac57a98dcfb0dbd60ca65936532dbd22aa95
Narendran Thangarajan <narendran.thangarajan@gmail.com>
2014-04-13 03:01:13 -0700

    Refactoring code for adding RPC write and read

--- kad_server/buddyd.py
+++ kad_server/buddyd.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import traceback
 import os.path
 import json
 import twisted
@@ -9,32 +10,44 @@ from kad_server.buddynode import BuddyNode
 from twisted.internet.protocol import Protocol, Factory
 
 class RPCServer(Protocol):
+  
+  def write_block(self):
+    pass
+
+  def read_block(self):
+    pass
+
+  def diskusage(self):
+    st = os.statvfs(".")
+    response = {}
+    response["total_bytes"] = st.f_blocks * st.f_frsize
+    response["free_bytes"] = st.f_bavail * st.f_frsize
+    response["used_bytes"] = (st.f_blocks - st.f_bfree) * st.f_frsize
+    self.transport.write(json.dumps(response))
+    return
+  
   def dataReceived(self, data):
     try:
       cmd = json.loads(data)
-      if(cmd["command"])=="du":
-        st = os.statvfs(".")
-        response = {}
-        response["total_bytes"] = st.f_blocks * st.f_frsize
-        response["free_bytes"] = st.f_bavail * st.f_frsize
-        response["used_bytes"] = (st.f_blocks - st.f_bfree) * st.f_frsize
-        self.transport.write(json.dumps(response))
+      command = cmd["command"]
+      if(command=="du"):
+        self.diskusage()
+      elif(command=="write"):
+        self.write_block()
+      elif(command=="read"):
+        self.read_block()
       else:
-        errorObj = {}
-        errorObj["type"] = "Error"
-        errorObj["reason"] = "Unsupported command"
-        self.transport.write(json.dumps(errorObj))
+        raise Exception("Unsupported operation")
     except :
+      traceback.print_exc(file=sys.stdout)
       errorObj = {}
       errorObj["type"] = "Error"
-      errorObj["reason"] = "Invalid Format"
+      errorObj["reason"] = "Invalid Request"
       self.transport.write(json.dumps(errorObj))
-
-    return
 
 factory = Factory()
 factory.protocol = RPCServer
-twisted.internet.reactor.listenTCP(7777, factory)
+twisted.internet.reactor.listenTCP(9000, factory)
 
 if __name__ == '__main__':
 
